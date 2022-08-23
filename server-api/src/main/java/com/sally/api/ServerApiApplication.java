63b20a1240065cfbb765129e51bb47ccb160e8ae
sally-ksh <sally.devz@gmail.com>
2022-08-23 21:13:22 +0900

    Fix #6 ServerApplication

--- server-api/src/main/java/com/sally/api/ServerApiApplication.java
+++ server-api/src/main/java/com/sally/api/ServerApiApplication.java
@@ -1,13 +1,13 @@
-package com.sally.server;
+package com.sally.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication.run(ServerApiApplication.class, args);
 	}
 
 }
