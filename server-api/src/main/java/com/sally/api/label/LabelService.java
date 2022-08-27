4366e3b8707ec9456f0cb82c2b89a2ef4cea72cc
sally-ksh <sally.devz@gmail.com>
2022-08-27 14:23:57 +0900

    Add #11 LabelService - verify of duplicated label name

--- server-api/src/main/java/com/sally/api/label/LabelService.java
+++ server-api/src/main/java/com/sally/api/label/LabelService.java
@@ -5,6 +5,7 @@ import com.sally.api.login.dto.AuthUser;
 import com.sally.api.project.ProjectService;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,7 +15,11 @@ public class LabelService {
 	private final LabelRepository labelRepository;
 	private final ProjectService projectService;
 
+	@Transactional
 	public void create(LabelRequest.Creation creation, String teamName, AuthUser authUser) {
+		if (labelRepository.existsByName(creation.getName())) {
+			throw new RuntimeException("Overlap : label");
+		}
 		Label label = creation.toEntity(projectService.getAndVerify(authUser.projectId(), teamName));
 		labelRepository.save(label);
 	}
