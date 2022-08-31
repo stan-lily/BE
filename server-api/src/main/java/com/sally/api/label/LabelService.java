b4b1d9bb688b9c2b5e0aeb39c3a9480f1f7bdc2c
sally-ksh <sally.devz@gmail.com>
2022-08-31 14:47:22 +0900

    Refactor And FIx #16 LabelService : Project -> ProjectInfo, change method getFromTeamName() in readAll()
    - DTO of project domain
    - the readAll method's logic does not need to verify team's member

--- server-api/src/main/java/com/sally/api/label/LabelService.java
+++ server-api/src/main/java/com/sally/api/label/LabelService.java
@@ -4,8 +4,8 @@ import com.sally.api.label.dto.LabelRequest;
 import com.sally.api.label.dto.LabelResponse;
 import com.sally.api.login.dto.AuthUser;
 import com.sally.api.login.dto.FakeAuthUser;
-import com.sally.api.project.Project;
 import com.sally.api.project.ProjectService;
+import com.sally.api.project.dto.ProjectInfo;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,11 +30,16 @@ public class LabelService {
 		labelRepository.save(label);
 	}
 
+	/**
+	 * 라벨 조회는 팀원이 아니어도 가능하다.
+	 * @param teamName
+	 * @return
+	 */
 	@Transactional(readOnly = true)
-	public LabelResponse.Lists readAll(String teamName, FakeAuthUser authUser) {
-		Project project = getProjectAndVerify(teamName, authUser);
+	public LabelResponse.Lists readAll(String teamName) {
+		ProjectInfo projectInfo = projectService.getFromTeamName(teamName);
 		return new LabelResponse.Lists(
-			labelRepository.findAllByProjectId(project.id()).stream()
+			labelRepository.findAllByProjectId(projectInfo.getId()).stream()
 				.map(LabelResponse.LabelDto::new)
 				.collect(Collectors.toList()));
 	}
@@ -42,8 +47,8 @@ public class LabelService {
 	@Transactional
 	public void update(Long labelId, LabelRequest.LabelDto labelDto, String teamName,
 		FakeAuthUser authUser) {
-		Project project = getProjectAndVerify(teamName, authUser);
-		Label label = getLabelOrThrow(labelId, project);
+		ProjectInfo projectInfo = getProjectAndVerify(teamName, authUser);
+		Label label = getLabelOrThrow(labelId, projectInfo.getId());
 		label.update(
 			labelDto.getName(),
 			labelDto.getDescription(),
@@ -53,17 +58,17 @@ public class LabelService {
 
 	@Transactional
 	public void delete(Long labelId, String teamName, AuthUser authUser) {
-		Project project = getProjectAndVerify(teamName, authUser);
-		Label label = getLabelOrThrow(labelId, project);
+		ProjectInfo projectInfo = getProjectAndVerify(teamName, authUser);
+		Label label = getLabelOrThrow(labelId, projectInfo.getId());
 		label.delete();
 	}
 
-	private Label getLabelOrThrow(Long labelId, Project project) {
-		return labelRepository.findByIdAndProjectId(labelId, project.id())
+	private Label getLabelOrThrow(Long labelId, Long projectId) {
+		return labelRepository.findByIdAndProjectId(labelId, projectId)
 			.orElseThrow(() -> new NoSuchElementException("no existed label"));
 	}
 
-	private Project getProjectAndVerify(String teamName, AuthUser authUser) {
+	private ProjectInfo getProjectAndVerify(String teamName, AuthUser authUser) {
 		return projectService.getAndVerify(authUser.projectId(), teamName);
 	}
 }
