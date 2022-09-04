b2b88c1caeaa21cc58d01a20985e37f4c211835f
sally-ksh <sally.devz@gmail.com>
2022-09-04 15:05:17 +0900

    Fix AssembleRequest - test with AOS

--- server-api/src/main/java/com/sally/api/assemble/dto/AssembleRequest.java
+++ server-api/src/main/java/com/sally/api/assemble/dto/AssembleRequest.java
@@ -7,8 +7,6 @@ import com.sally.api.util.Period;
 
 import java.time.LocalDate;
 
-import javax.validation.constraints.Future;
-import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -24,9 +22,9 @@ public class AssembleRequest {
 		@NotBlank(message = "어셈블 제목을 입력 하세요.")
 		@Size(max = 100, message = "어셈블 제목은 100자 이내 입니다.")
 		private String assembleTitle;
-		@FutureOrPresent(message = "오늘 이후 부터 등록 가능합니다.")
+		// @FutureOrPresent(message = "오늘 이후 부터 등록 가능합니다.")
 		private LocalDate assembleStartAt;
-		@Future(message = "오늘 이후 부터 등록 가능합니다.")
+		// @Future(message = "오늘 이후 부터 등록 가능합니다.")
 		private LocalDate assembleEndAt;
 
 		public Assemble toEntity(ProjectInfo projectInfo) {
