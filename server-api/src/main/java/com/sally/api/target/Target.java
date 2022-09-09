06f31d1b002822ce191b70127b4fe54b7c181549
sally-ksh <sally.devz@gmail.com>
2022-09-10 01:59:58 +0900

    Add #28 Target

---
+++ server-api/src/main/java/com/sally/api/target/Target.java
@@ -0,0 +1,82 @@
+package com.sally.api.target;
+
+import com.sally.api.assemble.Assemble;
+import com.sally.api.label.Label;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = "id")
+@Where(clause = "is_deleted = false")
+@Table(name = "tb_target")
+@Entity
+public class Target {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private LocalDate endAt;
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "assemble_day_id")
+	private Assemble assemble;
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "label_id")
+	private Label label;
+	@ColumnDefault("0")
+	private boolean isDeleted = false;
+	private LocalDateTime createAt;
+	private LocalDateTime updatedAt;
+
+	@Builder(access = AccessLevel.PRIVATE)
+	public Target(Long id, String title, LocalDate endAt, Assemble assembleDay, Label label,
+		LocalDateTime createAt, LocalDateTime updatedAt) {
+		this.id = id;
+		this.title = title;
+		this.endAt = endAt;
+		this.assemble = assembleDay;
+		this.label = label;
+		this.createAt = createAt;
+		this.updatedAt = updatedAt;
+	}
+
+	public static Target createOf(String title, LocalDate endAt, Assemble assemble, Label label) {
+		return Target.builder()
+			.title(title)
+			.endAt(endAt)
+			.assembleDay(assemble)
+			.label(label)
+			.createAt(LocalDateTime.now())
+			.updatedAt(LocalDateTime.now())
+			.build();
+	}
+
+	public Long id() {
+		return id;
+	}
+
+	public LocalDate day() {
+		return endAt;
+	}
+
+	public Long labelId() {
+		return label.id();
+	}
+}
