2f727480402fe2d811144a494a1c281294fcee83
sally-ksh <sally.devz@gmail.com>
2022-12-09 21:47:33 +0900

    Add #40 TargetNotificationRepository

---
+++ server-api/src/main/java/com/sally/api/targetnotification/TargetNotificationRepository.java
@@ -0,0 +1,6 @@
+package com.sally.api.targetnotification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TargetNotificationRepository extends JpaRepository<TargetNotification, Long> {
+}
